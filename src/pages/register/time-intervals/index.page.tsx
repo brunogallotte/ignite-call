af0714898bed49af84b3fe8eae1dd7fdbbcc7af1
Bruno Gallotte <brunogallotte1@gmail.com>
2024-02-12 16:43:09 -0300

    Feat: add component calendar

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -21,6 +21,8 @@ import { z } from 'zod'
 import { getWeekDays } from '@/utils/get-week-days'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { convertTimeStringToMinutes } from '@/utils/convert-time-string-to-minutes'
+import { api } from '@/lib/axios'
+import { useRouter } from 'next/router'
 
 const timeIntervalsForSchema = z.object({
   intervals: z
@@ -85,6 +87,8 @@ export default function TimeIntervals() {
     },
   })
 
+  const router = useRouter()
+
   const weekDays = getWeekDays()
 
   const { fields } = useFieldArray({
@@ -95,8 +99,13 @@ export default function TimeIntervals() {
   const intervals = watch('intervals')
 
   async function handleSetTimeIntervals(data: any) {
-    const formData = data as TimeIntervalsFormOutput
-    console.log(formData)
+    const { intervals } = data as TimeIntervalsFormOutput
+
+    await api.post('/users/time-intervals', {
+      intervals,
+    })
+
+    await router.push('/register/update-profile')
   }
 
   return (
---
+++ src/pages/schedule/[username]/scheduleForm/index.tsx
@@ -0,0 +1,5 @@
+import { CalendarStep } from './CalendarSetp'
+
+export function ScheduleForm() {
+  return <CalendarStep></CalendarStep>
+}
