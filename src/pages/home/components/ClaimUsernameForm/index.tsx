b931072ac3cc58e59b45873cecaa67de021ca0af
Bruno Gallotte <brunogallotte1@gmail.com>
2024-02-15 08:19:47 -0300

    Fix: add property crossorigin on component TextInput

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -39,6 +39,7 @@ export function ClaimUsernameForm() {
     <>
       <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
         <TextInput
+          crossOrigin={undefined}
           size="sm"
           prefix="ignite.com/"
           placeholder="seu-usuario"
