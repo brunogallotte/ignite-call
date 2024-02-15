75db8db4152645fc7aa640a91913f06682705ff4
Bruno Gallotte <brunogallotte1@gmail.com>
2024-02-15 08:22:37 -0300

    Fix: add property crossorigin on component TextInput register

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -79,6 +79,7 @@ export default function Register() {
           <label>
             <Text size="sm">Nome de usuário</Text>
             <TextInput
+              crossOrigin={undefined}
               prefix="ignite.com/"
               placeholder="seu-usuario"
               {...register('username')}
@@ -90,7 +91,11 @@ export default function Register() {
 
           <label>
             <Text size="sm">Nome completo</Text>
-            <TextInput placeholder="Seu nome" {...register('name')} />
+            <TextInput
+              crossOrigin={undefined}
+              placeholder="Seu nome"
+              {...register('name')}
+            />
             {errors.name && (
               <FormError size="sm">{errors.name.message}</FormError>
             )}
